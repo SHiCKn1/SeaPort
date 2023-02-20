1492126d7f420e802007bc60389f1ef592e9fc6e
SHiCKn1 <SHiCKn1@gmail.com>
2023-02-20 18:30:47 +0300

    refactor

--- src/main/java/com/prokopovich/action/PortManager.java
+++ src/main/java/com/prokopovich/action/PortManager.java
@@ -6,7 +6,8 @@ import java.util.concurrent.TimeUnit;
 
 public class PortManager {
     public static void startWork() throws InterruptedException {
-        for (int i = 1; i <= 7; i++) {
+        int NumberOfShip = 7;
+        for (int i = 1; i <= NumberOfShip; i++) {
             //TODO переделать под ExecutorService
             new Thread(new Ship("Ship №" + i,i)).start();
             TimeUnit.MILLISECONDS.sleep(400);
